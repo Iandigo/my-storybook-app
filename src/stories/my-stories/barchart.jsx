a44a3cdc3fbafbfc5af35f71ab374207e93fb8fe
nguyentien <nguyentien@deakin.edu.au>
2021-07-08 15:01:38 +1000

    testing

---
+++ src/stories/my-stories/barchart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Pie, defaults } from 'react-chartjs-2'
+
+defaults.global.tooltips.enabled = false
+defaults.global.legend.position = 'bottom'
+
+const BarChart = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
---
+++ src/stories/my-stories/chart.stories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Barchart } from "./barchart";
+
+export default {
+    title: "chart",
+    component: Barchart,
+
+};
+
+export const Piechart = () => <Barchart/>;
